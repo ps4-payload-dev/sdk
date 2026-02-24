377ee7c57cc10c017074fa562c3163a2fcf869c7
John Törnblom <john.tornblom@gmail.com>
2026-02-24 15:53:27 +0100

    sce stubs: add SceFsInternalForVsh

---
+++ sce_stubs/libSceFsInternalForVsh.c
@@ -0,0 +1,528 @@
+
+asm(".global sceBdSchedCancelBackgroundCopyRequest\n"
+    ".type sceBdSchedCancelBackgroundCopyRequest @function\n"
+    "sceBdSchedCancelBackgroundCopyRequest:\n");
+
+asm(".global sceBdSchedCancelPrioritizedBackgroundCopyRequest\n"
+    ".type sceBdSchedCancelPrioritizedBackgroundCopyRequest @function\n"
+    "sceBdSchedCancelPrioritizedBackgroundCopyRequest:\n");
+
+asm(".global sceBdSchedConfigure\n"
+    ".type sceBdSchedConfigure @function\n"
+    "sceBdSchedConfigure:\n");
+
+asm(".global sceBdSchedConfigure2\n"
+    ".type sceBdSchedConfigure2 @function\n"
+    "sceBdSchedConfigure2:\n");
+
+asm(".global sceBdSchedDeconfigure\n"
+    ".type sceBdSchedDeconfigure @function\n"
+    "sceBdSchedDeconfigure:\n");
+
+asm(".global sceBdSchedGetBackgroundCopyRequest\n"
+    ".type sceBdSchedGetBackgroundCopyRequest @function\n"
+    "sceBdSchedGetBackgroundCopyRequest:\n");
+
+asm(".global sceBdSchedGetExtentMap\n"
+    ".type sceBdSchedGetExtentMap @function\n"
+    "sceBdSchedGetExtentMap:\n");
+
+asm(".global sceBdSchedGetPrioritizedBackgroundCopyRequest\n"
+    ".type sceBdSchedGetPrioritizedBackgroundCopyRequest @function\n"
+    "sceBdSchedGetPrioritizedBackgroundCopyRequest:\n");
+
+asm(".global sceBdSchedGetState\n"
+    ".type sceBdSchedGetState @function\n"
+    "sceBdSchedGetState:\n");
+
+asm(".global sceBdSchedSetBackgroundCopyRequest\n"
+    ".type sceBdSchedSetBackgroundCopyRequest @function\n"
+    "sceBdSchedSetBackgroundCopyRequest:\n");
+
+asm(".global sceBdSchedSetDiscIdentity\n"
+    ".type sceBdSchedSetDiscIdentity @function\n"
+    "sceBdSchedSetDiscIdentity:\n");
+
+asm(".global sceBdSchedSetPrioritizedBackgroundCopyRequest\n"
+    ".type sceBdSchedSetPrioritizedBackgroundCopyRequest @function\n"
+    "sceBdSchedSetPrioritizedBackgroundCopyRequest:\n");
+
+asm(".global sceBdSchedStartGame\n"
+    ".type sceBdSchedStartGame @function\n"
+    "sceBdSchedStartGame:\n");
+
+asm(".global sceBdSchedStartRipping\n"
+    ".type sceBdSchedStartRipping @function\n"
+    "sceBdSchedStartRipping:\n");
+
+asm(".global sceBdSchedStopScheduling\n"
+    ".type sceBdSchedStopScheduling @function\n"
+    "sceBdSchedStopScheduling:\n");
+
+asm(".global sceFsCheckFormat\n"
+    ".type sceFsCheckFormat @function\n"
+    "sceFsCheckFormat:\n");
+
+asm(".global sceFsCheckSingleUser\n"
+    ".type sceFsCheckSingleUser @function\n"
+    "sceFsCheckSingleUser:\n");
+
+asm(".global sceFsCreateExfatPartition\n"
+    ".type sceFsCreateExfatPartition @function\n"
+    "sceFsCreateExfatPartition:\n");
+
+asm(".global sceFsCreatePfsSaveDataImage\n"
+    ".type sceFsCreatePfsSaveDataImage @function\n"
+    "sceFsCreatePfsSaveDataImage:\n");
+
+asm(".global sceFsCreatePfsTrophyDataImage\n"
+    ".type sceFsCreatePfsTrophyDataImage @function\n"
+    "sceFsCreatePfsTrophyDataImage:\n");
+
+asm(".global sceFsDeviceAlignedPread\n"
+    ".type sceFsDeviceAlignedPread @function\n"
+    "sceFsDeviceAlignedPread:\n");
+
+asm(".global sceFsDeviceAlignedPwrite\n"
+    ".type sceFsDeviceAlignedPwrite @function\n"
+    "sceFsDeviceAlignedPwrite:\n");
+
+asm(".global sceFsErriCtl\n"
+    ".type sceFsErriCtl @function\n"
+    "sceFsErriCtl:\n");
+
+asm(".global sceFsErriGetState\n"
+    ".type sceFsErriGetState @function\n"
+    "sceFsErriGetState:\n");
+
+asm(".global sceFsExfatMkfsExternalHdd\n"
+    ".type sceFsExfatMkfsExternalHdd @function\n"
+    "sceFsExfatMkfsExternalHdd:\n");
+
+asm(".global sceFsExfatfsMkfs\n"
+    ".type sceFsExfatfsMkfs @function\n"
+    "sceFsExfatfsMkfs:\n");
+
+asm(".global sceFsExtUSBSchedDestroy\n"
+    ".type sceFsExtUSBSchedDestroy @function\n"
+    "sceFsExtUSBSchedDestroy:\n");
+
+asm(".global sceFsExtUSBSchedInsert\n"
+    ".type sceFsExtUSBSchedInsert @function\n"
+    "sceFsExtUSBSchedInsert:\n");
+
+asm(".global sceFsExtUSBSchedInsertForHDD\n"
+    ".type sceFsExtUSBSchedInsertForHDD @function\n"
+    "sceFsExtUSBSchedInsertForHDD:\n");
+
+asm(".global sceFsExternalStorageDeconfigure\n"
+    ".type sceFsExternalStorageDeconfigure @function\n"
+    "sceFsExternalStorageDeconfigure:\n");
+
+asm(".global sceFsExternalStorageGetRawDevice\n"
+    ".type sceFsExternalStorageGetRawDevice @function\n"
+    "sceFsExternalStorageGetRawDevice:\n");
+
+asm(".global sceFsExternalStorageInitialize\n"
+    ".type sceFsExternalStorageInitialize @function\n"
+    "sceFsExternalStorageInitialize:\n");
+
+asm(".global sceFsExternalStorageVerifyMetadata\n"
+    ".type sceFsExternalStorageVerifyMetadata @function\n"
+    "sceFsExternalStorageVerifyMetadata:\n");
+
+asm(".global sceFsGetAttribute\n"
+    ".type sceFsGetAttribute @function\n"
+    "sceFsGetAttribute:\n");
+
+asm(".global sceFsGetClusterStatistics\n"
+    ".type sceFsGetClusterStatistics @function\n"
+    "sceFsGetClusterStatistics:\n");
+
+asm(".global sceFsGetDeviceSectorsize\n"
+    ".type sceFsGetDeviceSectorsize @function\n"
+    "sceFsGetDeviceSectorsize:\n");
+
+asm(".global sceFsGetSmrInfo\n"
+    ".type sceFsGetSmrInfo @function\n"
+    "sceFsGetSmrInfo:\n");
+
+asm(".global sceFsGschedCtl\n"
+    ".type sceFsGschedCtl @function\n"
+    "sceFsGschedCtl:\n");
+
+asm(".global sceFsISSchedConfigCurrentThread\n"
+    ".type sceFsISSchedConfigCurrentThread @function\n"
+    "sceFsISSchedConfigCurrentThread:\n");
+
+asm(".global sceFsISSchedConfigDescriptor\n"
+    ".type sceFsISSchedConfigDescriptor @function\n"
+    "sceFsISSchedConfigDescriptor:\n");
+
+asm(".global sceFsISSchedConfigThread\n"
+    ".type sceFsISSchedConfigThread @function\n"
+    "sceFsISSchedConfigThread:\n");
+
+asm(".global sceFsISSchedMapStat\n"
+    ".type sceFsISSchedMapStat @function\n"
+    "sceFsISSchedMapStat:\n");
+
+asm(".global sceFsISSchedResultToPriority\n"
+    ".type sceFsISSchedResultToPriority @function\n"
+    "sceFsISSchedResultToPriority:\n");
+
+asm(".global sceFsISSchedResultToSlot\n"
+    ".type sceFsISSchedResultToSlot @function\n"
+    "sceFsISSchedResultToSlot:\n");
+
+asm(".global sceFsISSchedStatCurrentThread\n"
+    ".type sceFsISSchedStatCurrentThread @function\n"
+    "sceFsISSchedStatCurrentThread:\n");
+
+asm(".global sceFsISSchedStatDescriptor\n"
+    ".type sceFsISSchedStatDescriptor @function\n"
+    "sceFsISSchedStatDescriptor:\n");
+
+asm(".global sceFsISSchedStatThread\n"
+    ".type sceFsISSchedStatThread @function\n"
+    "sceFsISSchedStatThread:\n");
+
+asm(".global sceFsISSchedUnmapStat\n"
+    ".type sceFsISSchedUnmapStat @function\n"
+    "sceFsISSchedUnmapStat:\n");
+
+asm(".global sceFsInitCreatePfsSaveDataOpt\n"
+    ".type sceFsInitCreatePfsSaveDataOpt @function\n"
+    "sceFsInitCreatePfsSaveDataOpt:\n");
+
+asm(".global sceFsInitCreatePfsTrophyDataOpt\n"
+    ".type sceFsInitCreatePfsTrophyDataOpt @function\n"
+    "sceFsInitCreatePfsTrophyDataOpt:\n");
+
+asm(".global sceFsInitMountDownloadDataOpt\n"
+    ".type sceFsInitMountDownloadDataOpt @function\n"
+    "sceFsInitMountDownloadDataOpt:\n");
+
+asm(".global sceFsInitMountGamePkgOpt\n"
+    ".type sceFsInitMountGamePkgOpt @function\n"
+    "sceFsInitMountGamePkgOpt:\n");
+
+asm(".global sceFsInitMountLwfsCompatOpt\n"
+    ".type sceFsInitMountLwfsCompatOpt @function\n"
+    "sceFsInitMountLwfsCompatOpt:\n");
+
+asm(".global sceFsInitMountLwfsOpt\n"
+    ".type sceFsInitMountLwfsOpt @function\n"
+    "sceFsInitMountLwfsOpt:\n");
+
+asm(".global sceFsInitMountSaveDataOpt\n"
+    ".type sceFsInitMountSaveDataOpt @function\n"
+    "sceFsInitMountSaveDataOpt:\n");
+
+asm(".global sceFsInitMountTrophyDataOpt\n"
+    ".type sceFsInitMountTrophyDataOpt @function\n"
+    "sceFsInitMountTrophyDataOpt:\n");
+
+asm(".global sceFsInitUmountDownloadDataOpt\n"
+    ".type sceFsInitUmountDownloadDataOpt @function\n"
+    "sceFsInitUmountDownloadDataOpt:\n");
+
+asm(".global sceFsInitUmountGamePkgOpt\n"
+    ".type sceFsInitUmountGamePkgOpt @function\n"
+    "sceFsInitUmountGamePkgOpt:\n");
+
+asm(".global sceFsInitUmountLwfsCompatOpt\n"
+    ".type sceFsInitUmountLwfsCompatOpt @function\n"
+    "sceFsInitUmountLwfsCompatOpt:\n");
+
+asm(".global sceFsInitUmountLwfsOpt\n"
+    ".type sceFsInitUmountLwfsOpt @function\n"
+    "sceFsInitUmountLwfsOpt:\n");
+
+asm(".global sceFsInitUmountSaveDataOpt\n"
+    ".type sceFsInitUmountSaveDataOpt @function\n"
+    "sceFsInitUmountSaveDataOpt:\n");
+
+asm(".global sceFsInitUmountTrophyDataOpt\n"
+    ".type sceFsInitUmountTrophyDataOpt @function\n"
+    "sceFsInitUmountTrophyDataOpt:\n");
+
+asm(".global sceFsLvdAttach\n"
+    ".type sceFsLvdAttach @function\n"
+    "sceFsLvdAttach:\n");
+
+asm(".global sceFsLvdAttach2\n"
+    ".type sceFsLvdAttach2 @function\n"
+    "sceFsLvdAttach2:\n");
+
+asm(".global sceFsLvdAttachPartiallyCryptedDownloadData\n"
+    ".type sceFsLvdAttachPartiallyCryptedDownloadData @function\n"
+    "sceFsLvdAttachPartiallyCryptedDownloadData:\n");
+
+asm(".global sceFsLvdAttachSingleDefaultImage\n"
+    ".type sceFsLvdAttachSingleDefaultImage @function\n"
+    "sceFsLvdAttachSingleDefaultImage:\n");
+
+asm(".global sceFsLvdAttachSingleImage\n"
+    ".type sceFsLvdAttachSingleImage @function\n"
+    "sceFsLvdAttachSingleImage:\n");
+
+asm(".global sceFsLvdDetach\n"
+    ".type sceFsLvdDetach @function\n"
+    "sceFsLvdDetach:\n");
+
+asm(".global sceFsLvdDisableLayer\n"
+    ".type sceFsLvdDisableLayer @function\n"
+    "sceFsLvdDisableLayer:\n");
+
+asm(".global sceFsLvdEnableLayer\n"
+    ".type sceFsLvdEnableLayer @function\n"
+    "sceFsLvdEnableLayer:\n");
+
+asm(".global sceFsLvdSetDiscIdentity\n"
+    ".type sceFsLvdSetDiscIdentity @function\n"
+    "sceFsLvdSetDiscIdentity:\n");
+
+asm(".global sceFsLvdWaitResumed\n"
+    ".type sceFsLvdWaitResumed @function\n"
+    "sceFsLvdWaitResumed:\n");
+
+asm(".global sceFsMlfsMount\n"
+    ".type sceFsMlfsMount @function\n"
+    "sceFsMlfsMount:\n");
+
+asm(".global sceFsMlfsMountWithFlags\n"
+    ".type sceFsMlfsMountWithFlags @function\n"
+    "sceFsMlfsMountWithFlags:\n");
+
+asm(".global sceFsMlfsUmount\n"
+    ".type sceFsMlfsUmount @function\n"
+    "sceFsMlfsUmount:\n");
+
+asm(".global sceFsMountDownloadData\n"
+    ".type sceFsMountDownloadData @function\n"
+    "sceFsMountDownloadData:\n");
+
+asm(".global sceFsMountGamePkg\n"
+    ".type sceFsMountGamePkg @function\n"
+    "sceFsMountGamePkg:\n");
+
+asm(".global sceFsMountLwfs\n"
+    ".type sceFsMountLwfs @function\n"
+    "sceFsMountLwfs:\n");
+
+asm(".global sceFsMountLwfsCompat\n"
+    ".type sceFsMountLwfsCompat @function\n"
+    "sceFsMountLwfsCompat:\n");
+
+asm(".global sceFsMountPartiallyCryptedDownloadData\n"
+    ".type sceFsMountPartiallyCryptedDownloadData @function\n"
+    "sceFsMountPartiallyCryptedDownloadData:\n");
+
+asm(".global sceFsMountSaveData\n"
+    ".type sceFsMountSaveData @function\n"
+    "sceFsMountSaveData:\n");
+
+asm(".global sceFsMountTrophyData\n"
+    ".type sceFsMountTrophyData @function\n"
+    "sceFsMountTrophyData:\n");
+
+asm(".global sceFsReserveCluster\n"
+    ".type sceFsReserveCluster @function\n"
+    "sceFsReserveCluster:\n");
+
+asm(".global sceFsSetAttribute\n"
+    ".type sceFsSetAttribute @function\n"
+    "sceFsSetAttribute:\n");
+
+asm(".global sceFsSetClusterCacheSize\n"
+    ".type sceFsSetClusterCacheSize @function\n"
+    "sceFsSetClusterCacheSize:\n");
+
+asm(".global sceFsSetFileReservedTotal\n"
+    ".type sceFsSetFileReservedTotal @function\n"
+    "sceFsSetFileReservedTotal:\n");
+
+asm(".global sceFsSetSystemReserveSize\n"
+    ".type sceFsSetSystemReserveSize @function\n"
+    "sceFsSetSystemReserveSize:\n");
+
+asm(".global sceFsSmrTrim\n"
+    ".type sceFsSmrTrim @function\n"
+    "sceFsSmrTrim:\n");
+
+asm(".global sceFsSmrTrimAll\n"
+    ".type sceFsSmrTrimAll @function\n"
+    "sceFsSmrTrimAll:\n");
+
+asm(".global sceFsSmrTrimPath\n"
+    ".type sceFsSmrTrimPath @function\n"
+    "sceFsSmrTrimPath:\n");
+
+asm(".global sceFsTrophyImageError\n"
+    ".type sceFsTrophyImageError @function\n"
+    "sceFsTrophyImageError:\n");
+
+asm(".global sceFsUfsAllocateAdditionalContents\n"
+    ".type sceFsUfsAllocateAdditionalContents @function\n"
+    "sceFsUfsAllocateAdditionalContents:\n");
+
+asm(".global sceFsUfsAllocateAligned\n"
+    ".type sceFsUfsAllocateAligned @function\n"
+    "sceFsUfsAllocateAligned:\n");
+
+asm(".global sceFsUfsAllocateDownloadData\n"
+    ".type sceFsUfsAllocateDownloadData @function\n"
+    "sceFsUfsAllocateDownloadData:\n");
+
+asm(".global sceFsUfsAllocateGameImage\n"
+    ".type sceFsUfsAllocateGameImage @function\n"
+    "sceFsUfsAllocateGameImage:\n");
+
+asm(".global sceFsUfsAllocatePatchImage\n"
+    ".type sceFsUfsAllocatePatchImage @function\n"
+    "sceFsUfsAllocatePatchImage:\n");
+
+asm(".global sceFsUfsAllocateSaveData\n"
+    ".type sceFsUfsAllocateSaveData @function\n"
+    "sceFsUfsAllocateSaveData:\n");
+
+asm(".global sceFsUfsAllocateTrophyData\n"
+    ".type sceFsUfsAllocateTrophyData @function\n"
+    "sceFsUfsAllocateTrophyData:\n");
+
+asm(".global sceFsUfsCheckCleanFlag\n"
+    ".type sceFsUfsCheckCleanFlag @function\n"
+    "sceFsUfsCheckCleanFlag:\n");
+
+asm(".global sceFsUfsCheckFixedCylinderGroupSize\n"
+    ".type sceFsUfsCheckFixedCylinderGroupSize @function\n"
+    "sceFsUfsCheckFixedCylinderGroupSize:\n");
+
+asm(".global sceFsUfsCoerceCryptMode\n"
+    ".type sceFsUfsCoerceCryptMode @function\n"
+    "sceFsUfsCoerceCryptMode:\n");
+
+asm(".global sceFsUfsCollectTelemetry\n"
+    ".type sceFsUfsCollectTelemetry @function\n"
+    "sceFsUfsCollectTelemetry:\n");
+
+asm(".global sceFsUfsCollectTelemetry2\n"
+    ".type sceFsUfsCollectTelemetry2 @function\n"
+    "sceFsUfsCollectTelemetry2:\n");
+
+asm(".global sceFsUfsDirectWrite\n"
+    ".type sceFsUfsDirectWrite @function\n"
+    "sceFsUfsDirectWrite:\n");
+
+asm(".global sceFsUfsEstimateZoneCapacityForUser\n"
+    ".type sceFsUfsEstimateZoneCapacityForUser @function\n"
+    "sceFsUfsEstimateZoneCapacityForUser:\n");
+
+asm(".global sceFsUfsExtendFile\n"
+    ".type sceFsUfsExtendFile @function\n"
+    "sceFsUfsExtendFile:\n");
+
+asm(".global sceFsUfsFsck\n"
+    ".type sceFsUfsFsck @function\n"
+    "sceFsUfsFsck:\n");
+
+asm(".global sceFsUfsFsckWithMM\n"
+    ".type sceFsUfsFsckWithMM @function\n"
+    "sceFsUfsFsckWithMM:\n");
+
+asm(".global sceFsUfsGetExtentMap\n"
+    ".type sceFsUfsGetExtentMap @function\n"
+    "sceFsUfsGetExtentMap:\n");
+
+asm(".global sceFsUfsGrowLwfs\n"
+    ".type sceFsUfsGrowLwfs @function\n"
+    "sceFsUfsGrowLwfs:\n");
+
+asm(".global sceFsUfsGrowfs\n"
+    ".type sceFsUfsGrowfs @function\n"
+    "sceFsUfsGrowfs:\n");
+
+asm(".global sceFsUfsHideBlocksFromUser\n"
+    ".type sceFsUfsHideBlocksFromUser @function\n"
+    "sceFsUfsHideBlocksFromUser:\n");
+
+asm(".global sceFsUfsInitalizeSparseFile\n"
+    ".type sceFsUfsInitalizeSparseFile @function\n"
+    "sceFsUfsInitalizeSparseFile:\n");
+
+asm(".global sceFsUfsLargewriteFsck\n"
+    ".type sceFsUfsLargewriteFsck @function\n"
+    "sceFsUfsLargewriteFsck:\n");
+
+asm(".global sceFsUfsLargewriteFsckWithMM\n"
+    ".type sceFsUfsLargewriteFsckWithMM @function\n"
+    "sceFsUfsLargewriteFsckWithMM:\n");
+
+asm(".global sceFsUfsMkLwfs\n"
+    ".type sceFsUfsMkLwfs @function\n"
+    "sceFsUfsMkLwfs:\n");
+
+asm(".global sceFsUfsMkfs\n"
+    ".type sceFsUfsMkfs @function\n"
+    "sceFsUfsMkfs:\n");
+
+asm(".global sceFsUfsMkfsForExternalHDD\n"
+    ".type sceFsUfsMkfsForExternalHDD @function\n"
+    "sceFsUfsMkfsForExternalHDD:\n");
+
+asm(".global sceFsUfsMkfsWithFixedCylinderGroupSize\n"
+    ".type sceFsUfsMkfsWithFixedCylinderGroupSize @function\n"
+    "sceFsUfsMkfsWithFixedCylinderGroupSize:\n");
+
+asm(".global sceFsUfsReallocateSparseFile\n"
+    ".type sceFsUfsReallocateSparseFile @function\n"
+    "sceFsUfsReallocateSparseFile:\n");
+
+asm(".global sceFsUfsRecryptBlocks\n"
+    ".type sceFsUfsRecryptBlocks @function\n"
+    "sceFsUfsRecryptBlocks:\n");
+
+asm(".global sceFsUfsRecryptGetState\n"
+    ".type sceFsUfsRecryptGetState @function\n"
+    "sceFsUfsRecryptGetState:\n");
+
+asm(".global sceFsUfsRecryptIsUnfinished\n"
+    ".type sceFsUfsRecryptIsUnfinished @function\n"
+    "sceFsUfsRecryptIsUnfinished:\n");
+
+asm(".global sceFsUfsRecryptSyncMetadata\n"
+    ".type sceFsUfsRecryptSyncMetadata @function\n"
+    "sceFsUfsRecryptSyncMetadata:\n");
+
+asm(".global sceFsUfsSeekHoleInFile\n"
+    ".type sceFsUfsSeekHoleInFile @function\n"
+    "sceFsUfsSeekHoleInFile:\n");
+
+asm(".global sceFsUfsStopOperation\n"
+    ".type sceFsUfsStopOperation @function\n"
+    "sceFsUfsStopOperation:\n");
+
+asm(".global sceFsUmountDownloadData\n"
+    ".type sceFsUmountDownloadData @function\n"
+    "sceFsUmountDownloadData:\n");
+
+asm(".global sceFsUmountGamePkg\n"
+    ".type sceFsUmountGamePkg @function\n"
+    "sceFsUmountGamePkg:\n");
+
+asm(".global sceFsUmountLwfs\n"
+    ".type sceFsUmountLwfs @function\n"
+    "sceFsUmountLwfs:\n");
+
+asm(".global sceFsUmountLwfsCompat\n"
+    ".type sceFsUmountLwfsCompat @function\n"
+    "sceFsUmountLwfsCompat:\n");
+
+asm(".global sceFsUmountSaveData\n"
+    ".type sceFsUmountSaveData @function\n"
+    "sceFsUmountSaveData:\n");
+
+asm(".global sceFsUmountTrophyData\n"
+    ".type sceFsUmountTrophyData @function\n"
+    "sceFsUmountTrophyData:\n");
